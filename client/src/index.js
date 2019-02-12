4c5557a3b3cf16792505f4e4149a1abaf70e90cf
codejoncode <jonathanjholloway@gmail.com>
2019-02-11 22:22:41 -0600

    created react app and set up folders in preparation for redux react router and other technologies

---
+++ client/src/index.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './Components/App';
+import * as serviceWorker from './serviceWorker';
+
+/*Hot modules allows for updating without the application having to refresh 
+ refresh manually if you have issues but most of the time you wont' need to refresh. 
+*/
+const rootElement = document.getElementById('root')
+
+let render = () => {
+    ReactDOM.render(<App />, rootElement)
+}
+
+if(module.hot){
+    module.hot.accept('./Components/App', () => {
+        setTimeout(render)
+    })
+}
+render() 
+
+
+// ReactDOM.render(<App />, document.getElementById('root'));
+
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: http://bit.ly/CRA-PWA
+serviceWorker.unregister();
