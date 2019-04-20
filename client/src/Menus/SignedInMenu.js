6a3ee1fa19b0d46057b85bff61e91dad1b75c3be
codejoncode <jonathanjholloway@gmail.com>
2019-04-19 20:54:59 -0500

    new  folder Menus which features two stateless componenets SignedInMenu and SignedOutMenu

---
+++ client/src/Menus/SignedInMenu.js
@@ -0,0 +1,11 @@
+import React from 'react' 
+
+const SignedInMenu = () => {
+    return (
+        <div>
+            <h2>Signed In Page</h2>
+        </div>
+    )
+}
+
+export default SignedInMenu; 
---
+++ client/src/Menus/SignedOutMenu.js
@@ -0,0 +1,11 @@
+import React from 'react' 
+
+const SignedOutMenu = () => {
+    return (
+        <div>
+            <h2>Signed Out Page</h2>
+        </div>
+    )
+}
+
+export default SignedOutMenu; 
