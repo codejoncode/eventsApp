c27f7c0dc639849c5f3298b59dc458051b0fcf60
codejoncode <jonathanjholloway@gmail.com>
2019-04-22 15:49:58 -0500

    made changes/ additions to add redux to the create event update event and delete event. Tested and the site is still functional

---
+++ client/src/Components/events/EventList/eventReducer.js
@@ -0,0 +1,88 @@
+import { createReducer  } from  '../../common/util/reducerUtil';
+import { CREATE_EVENT, DELETE_EVENT, UPDATE_EVENT } from './eventConstants';
+
+const initialState = [
+    {
+      id: "1",
+      title: "Trip to Tower of London",
+      date: "2018-03-27",
+      category: "culture",
+      description:
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus sollicitudin ligula eu leo tincidunt, quis scelerisque magna dapibus. Sed eget ipsum vel arcu vehicula ullamcorper.",
+      city: "London, UK",
+      venue: "Tower of London, St Katharine's & Wapping, London",
+      hostedBy: "Bob",
+      hostPhotoURL: "https://randomuser.me/api/portraits/men/20.jpg",
+      attendees: [
+        {
+          id: "a",
+          name: "Bob",
+          photoURL: "https://randomuser.me/api/portraits/men/20.jpg"
+        },
+        {
+          id: "b",
+          name: "Tom",
+          photoURL: "https://randomuser.me/api/portraits/men/22.jpg"
+        },
+        {
+          id: "d",
+          name: "Sally",
+          photoURL: "https://randomuser.me/api/portraits/women/42.jpg"
+        }
+      ]
+    },
+    {
+      id: "2",
+      title: "Trip to Punch and Judy Pub",
+      date: "2018-03-28",
+      category: "drinks",
+      description:
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus sollicitudin ligula eu leo tincidunt, quis scelerisque magna dapibus. Sed eget ipsum vel arcu vehicula ullamcorper.",
+      city: "London, UK",
+      venue: "Punch & Judy, Henrietta Street, London, UK",
+      hostedBy: "Tom",
+      hostPhotoURL: "https://randomuser.me/api/portraits/men/22.jpg",
+      attendees: [
+        {
+          id: "b",
+          name: "Tom",
+          photoURL: "https://randomuser.me/api/portraits/men/22.jpg"
+        },
+        {
+          id: "a",
+          name: "Bob",
+          photoURL: "https://randomuser.me/api/portraits/men/20.jpg"
+        },
+        {
+          id: "c",
+          name: "Samantha",
+          photoURL: "https://randomuser.me/api/portraits/women/20.jpg"
+        }
+      ]
+    }
+  ];
+
+
+  export const createEvent = (state, payload) => {
+      return [...initialState, Object.assign({}, payload.event)];
+  };
+
+  export const updateEvent = (state, payload) => {
+      return [
+          ...initialState.filter(event => event.id !== payload.event.id), // spread eveything but the event that is being updated 
+          Object.assign({}, payload.event)
+      ]
+  };
+
+  export const deleteEvent = (state, payload) => {
+      return [
+        ...initialState.filter(event => event.id !== payload.eventId),
+      ]
+  };
+
+
+  export default createReducer(initialState, {
+      [CREATE_EVENT]: createEvent, 
+      [UPDATE_EVENT]: updateEvent, 
+      [DELETE_EVENT]: deleteEvent, //  same as  let obj = {}   obj[DELETE_EVENT] = deleteEvent  new ES6 syntax. 
+  });
--- client/src/store/Reducers/rootReducer.js
+++ client/src/store/Reducers/rootReducer.js
@@ -1,9 +1,11 @@
 import { combineReducers } from 'redux';
 import testReducer from './testReducer';
+import eventReducer from '../../Components/events/EventList/eventReducer';
 
 
 const rootReducer = combineReducers({
-    test: testReducer // will be able to access testReducer using the name test. 
+    test: testReducer, // will be able to access testReducer using the name test. 
+    events: eventReducer,
 });
 
 export default rootReducer; 
