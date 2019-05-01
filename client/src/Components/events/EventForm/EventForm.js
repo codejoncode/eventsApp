098309199c3db41a31f309654decb2a02e85adb9
codejoncode <jonathanjholloway@gmail.com>
2019-04-30 19:17:07 -0500

    added validation to forms

--- client/src/Components/events/EventForm/EventForm.js
+++ client/src/Components/events/EventForm/EventForm.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import { connect } from "react-redux";
 import { reduxForm, Field } from "redux-form";
+import { composeValidators, combineValidators, isRequired, hasLengthGreaterThan} from 'revalidate';
 import cuid from "cuid";
 import { Segment, Form, Button, Grid, Header } from "semantic-ui-react";
 import { createEvent, updateEvent } from "../EventList/eventActions";
@@ -38,6 +39,18 @@ const category = [
     {key: 'travel', text: 'Travel', value: 'travel'},
 ];
 
+const validate = combineValidators({
+  title : isRequired({message: 'The event title is required'}),
+  category: isRequired({message: 'Please provide a category'}),
+  description: composeValidators(
+    isRequired({ message: 'Please enter a description'}),
+    hasLengthGreaterThan(9)({message: 'Description: needs to be at least 10'})
+  )(),
+  city: isRequired('city'),
+  venue: isRequired('venue')
+
+});
+
 class EventForm extends Component {
 
   onFormSubmit = values => {
@@ -58,6 +71,7 @@ class EventForm extends Component {
   };
 
   render() {
+    const {invalid, submitting, pristine} = this.props; 
     return (
       <Grid>
         <Grid.Column width={10}>
@@ -105,10 +119,10 @@ class EventForm extends Component {
                 component={TextInput}
                 placeholder="On what date will you have this event?"
               />
-              <Button positive type="submit">
+              <Button disabled={invalid || submitting || pristine} positive type="submit">
                 Submit
               </Button>
-              <Button onClick={this.props.history.goBack} type="button">
+              <Button  onClick={this.props.history.goBack}type="button">
                 Cancel
               </Button>
             </Form>
@@ -122,4 +136,4 @@ class EventForm extends Component {
 export default connect(
   mapState,
   actions
-)(reduxForm({ form: "eventForm", enableReinitialize: true })(EventForm));
+)(reduxForm({ form: "eventForm", enableReinitialize: true, validate })(EventForm));
