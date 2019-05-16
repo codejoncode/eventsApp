361043ada245d821261dcd84d95c21ac03f2839a
codejoncode <jonathanjholloway@gmail.com>
2019-05-15 20:36:44 -0500

    performed some reformatting but also set the photos to display from firestore storage

--- client/src/Components/user/Settings/PhotosPage.js
+++ client/src/Components/user/Settings/PhotosPage.js
@@ -1,138 +1,196 @@
-import React, {Component} from 'react';
-import { connect } from 'react-redux';
-import {Image, Segment, Header, Divider, Grid, Button, Card, Icon} from 'semantic-ui-react';
-import Dropzone from 'react-dropzone';
-import Cropper from 'react-cropper'; 
-import 'cropperjs/dist/cropper.css';
-import { toastr } from 'react-redux-toastr';
-import { uploadProfileImage } from '../userActions'
+import React, { Component } from "react";
+import { connect } from "react-redux";
+import { firestoreConnect } from "react-redux-firebase";
+import { compose } from "redux"; // deeply nested function transportion comman seperate higher ordered components (neatier)
+import {
+  Image,
+  Segment,
+  Header,
+  Divider,
+  Grid,
+  Button,
+  Card,
+  Icon
+} from "semantic-ui-react";
+import Dropzone from "react-dropzone";
+import Cropper from "react-cropper";
+import "cropperjs/dist/cropper.css";
+import { toastr } from "react-redux-toastr";
+import { uploadProfileImage } from "../userActions";
+
+const query = ({ auth }) => {
+  return [
+    {
+      collection: "users",
+      doc: auth.uid,
+      subcollections: [{ collection: "photos" }],
+      storeAs: "photos"
+    }
+  ];
+};
+
 const actions = {
   uploadProfileImage
-}
+};
+
+const mapState = state => ({
+  auth: state.firebase.auth,
+  profile: state.firebase.profile,
+  photos: state.firestore.ordered.photos
+});
+
 class PhotosPage extends Component {
-    state = {
-      files: [], 
-      fileName: '',
-      cropResult: null,
-      image: {}
+  state = {
+    files: [],
+    fileName: "",
+    cropResult: null,
+    image: {}
+  };
+
+  onDrop = files => {
+    this.setState({
+      files,
+      fileName: files[0].name
+    });
+  };
+
+  cropImage = () => {
+    if (typeof this.refs.cropper.getCroppedCanvas() === "undefined") {
+      return; //if we do not have an image that we can crop^
     }
 
-    onDrop = (files) => {
+    var isIE = /*@cc_on!@*/ false || !!document.documentMode;
+    // just to use it so I don't get a warning will work in
+    //an alternative at some point to the toBlob  called dataURL
+
+    //toBlob will not work for users using internet Explorer
+
+    this.refs.cropper.getCroppedCanvas().toBlob(blob => {
+      let imageURL = URL.createObjectURL(blob);
       this.setState({
-        files,
-        fileName: files[0].name
+        cropResult: imageURL,
+        image: blob,
+        isIE
+      });
+    }, "image/jpeg");
+  };
+
+  uploadImage = async () => {
+    try {
+      await this.props.uploadProfileImage(
+        this.state.image,
+        this.state.fileName
+      );
+      this.cancelCrop(); // remove preview once upload successful
+      toastr.success("Success!", "Photo Uploaded!");
+    } catch (error) {
+      toastr.error("Oops", error.message);
+    }
+  };
+
+  cancelCrop = () => {
+    this.setState({
+      files: [],
+      image: []
+    });
+  };
+
+  render() {
+    const { photos, profile } = this.props;
+    let filteredPhotos = []; 
+    if (photos) {
+      filteredPhotos = photos.filter(photo => {
+        return photo.url !== profile.photoURL
       })
     }
+    return (
+      <Segment>
+        <Header dividing size="large" content="Your Photos" />
+        <Grid>
+          <Grid.Row />
+          <Grid.Column width={4}>
+            <Header color="teal" sub content="Step 1 - Add Photo" />
+            <Dropzone onDrop={this.onDrop} multiple={false}>
+              <div style={{ paddingtop: "30px", textAlign: "center" }}>
+                <Icon name="upload" size="huge" />
+                <Header content="Drop image here or click to add" />
+              </div>
+            </Dropzone>
+          </Grid.Column>
+          <Grid.Column width={1} />
+          <Grid.Column width={4}>
+            <Header sub color="teal" content="Step 2 - Resize image" />
+            <Cropper
+              style={{ height: 200, width: "100%" }}
+              ref="cropper"
+              src={this.state.files[0] && this.state.files[0].preview}
+              aspectRatio={1}
+              viewMode={0}
+              dragMove="move"
+              guides={false}
+              scalable={true}
+              cropBoxMovable={true}
+              cropBoxResizable={true}
+              crop={this.cropImage}
+            />
+          </Grid.Column>
+          <Grid.Column width={1} />
+          <Grid.Column width={4}>
+            <Header sub color="teal" content="Step 3 - Preview and Upload" />
+            {this.state.files[0] && (
+              <div>
+                <Image
+                  style={{ minHeight: "200px", minWidth: "200px" }}
+                  src={this.state.cropResult}
+                />
+                <Button.Group>
+                  <Button
+                    onClick={this.uploadImage}
+                    style={{ width: "100px" }}
+                    positive
+                    icon="check"
+                  />
+                  <Button
+                    onClick={this.cancelCrop}
+                    style={{ width: "100px" }}
+                    icon="close"
+                  />
+                </Button.Group>
+              </div>
+            )}
+          </Grid.Column>
+        </Grid>
 
-    cropImage = () => {
-      if(typeof this.refs.cropper.getCroppedCanvas() === 'undefined'){
-        return; //if we do not have an image that we can crop^
-      }
+        <Divider />
+        <Header sub color="teal" content="All Photos" />
 
-      var isIE = /*@cc_on!@*/false || !!document.documentMode;
-      // just to use it so I don't get a warning will work in 
-      //an alternative at some point to the toBlob  called dataURL  
+        <Card.Group itemsPerRow={5}>
+          <Card>
+            <Image src={profile.photoURL} />
+            <Button positive>Main Photo</Button>
+          </Card>
+          {photos && filteredPhotos.map((photo) => (
+          <Card key = {photo.id}>
+            <Image src={photo.url} />
+            <div className="ui two buttons">
+              <Button basic color="green">
+                Main
+              </Button>
+              <Button basic icon="trash" color="red" />
+            </div>
+          </Card>
 
-      //toBlob will not work for users using internet Explorer  
-
-      this.refs.cropper.getCroppedCanvas().toBlob(blob => {
-        let imageURL = URL.createObjectURL(blob)
-        this.setState({
-          cropResult: imageURL,
-          image: blob,
-          isIE
-        })
-      }, 'image/jpeg');
-    }
-
-    uploadImage = async () => {
-      try {
-        await this.props.uploadProfileImage(this.state.image, this.state.fileName)
-        this.cancelCrop(); // remove preview once upload successful
-        toastr.success('Success!', 'Photo Uploaded!')
-      } catch (error){
-        toastr.error('Oops', error.message)
-      }
-    }
-
-    cancelCrop = () => {
-      this.setState({
-        files: [], 
-        image: []
-      })
-    }
-
-    render() {
-
-        return (
-            <Segment>
-                <Header dividing size='large' content='Your Photos' />
-                <Grid>
-                    <Grid.Row />
-                    <Grid.Column width={4}>
-                        <Header color='teal' sub content='Step 1 - Add Photo'/>
-                        <Dropzone onDrop ={this.onDrop} multiple={false}>
-                          <div style = {{paddingtop: '30px', textAlign:'center'}}>
-                            <Icon name='upload' size='huge'/>
-                            <Header content='Drop image here or click to add'/>
-                          </div>
-                        </Dropzone>
-                    </Grid.Column>
-                    <Grid.Column width={1} />
-                    <Grid.Column width={4}>
-                        <Header sub color='teal' content='Step 2 - Resize image' />
-                        <Cropper 
-                         style={{height: 200, width: '100%'}}
-                         ref='cropper'
-                         src={this.state.files[0] && this.state.files[0].preview}
-                         aspectRatio={1}
-                         viewMode={0}
-                         dragMove='move'
-                         guides={false}
-                         scalable={true}
-                         cropBoxMovable={true}
-                         cropBoxResizable={true}
-                         crop = {this.cropImage}
-                        />
-                    </Grid.Column>
-                    <Grid.Column width={1} />
-                    <Grid.Column width={4}>
-                        <Header sub color='teal' content='Step 3 - Preview and Upload' />
-                        { this.state.files[0] &&
-                        <div>
-                        <Image style={{minHeight: '200px', minWidth: '200px'}} src={this.state.cropResult}/>
-                        <Button.Group>
-                          <Button onClick = {this.uploadImage} style = {{width: '100px'}} positive icon='check'/>
-                          <Button onClick = {this.cancelCrop} style = {{width: '100px'}}  icon='close'/>
-                        </Button.Group>
-                        </div>
-                        }
-                    </Grid.Column>
-
-                </Grid>
-
-                <Divider/>
-                <Header sub color='teal' content='All Photos'/>
-
-                <Card.Group itemsPerRow={5}>
-                    <Card>
-                        <Image src='https://randomuser.me/api/portraits/men/20.jpg'/>
-                        <Button positive>Main Photo</Button>
-                    </Card>
-
-                        <Card >
-                            <Image
-                                src='https://randomuser.me/api/portraits/men/20.jpg'
-                            />
-                            <div className='ui two buttons'>
-                                <Button basic color='green'>Main</Button>
-                                <Button basic icon='trash' color='red' />
-                            </div>
-                        </Card>
-                </Card.Group>
-            </Segment>
-        );
-    }
+          ))}
+        </Card.Group>
+      </Segment>
+    );
+  }
 }
 
-export default connect(null, actions)(PhotosPage);
+export default compose(
+  connect(
+    mapState,
+    actions
+  ),
+  firestoreConnect(auth => query(auth))
+)(PhotosPage);
--- client/src/Components/user/userActions.js
+++ client/src/Components/user/userActions.js
@@ -1,64 +1,76 @@
-import moment from 'moment'; 
-import { toastr } from 'react-redux-toastr';
-export const updateProfile = (user) => 
-  async(dispatch, getState, {getFirebase}) => {
-      const firebase = getFirebase();
-      //same as the loadAsh omit
-      const {isLoaded, isEmpty, ...updatedUser} = user; //updatedUser will be everything except for isLoaded and isEmpty. 
-      if(updatedUser.dateOfBirth !== getState().firebase.profile.dateOfBirth){
-          //convert into a javascript date.  only if they are different than what's on state. 
-          updatedUser.dateOfBirth = moment(updatedUser.dateOfBirth).toDate(); 
-      }
+import moment from "moment";
+import { toastr } from "react-redux-toastr";
+import cuid from "cuid";
 
-      try {
-        await firebase.updateProfile(updatedUser); 
-        toastr.success('Success', 'Profile updated!')
-      } catch (error) {
-          console.log(error); 
-      }
+export const updateProfile = user => async (
+  dispatch,
+  getState,
+  { getFirebase }
+) => {
+  const firebase = getFirebase();
+  //same as the loadAsh omit
+  const { isLoaded, isEmpty, ...updatedUser } = user; //updatedUser will be everything except for isLoaded and isEmpty.
+  if (updatedUser.dateOfBirth !== getState().firebase.profile.dateOfBirth) {
+    //convert into a javascript date.  only if they are different than what's on state.
+    updatedUser.dateOfBirth = moment(updatedUser.dateOfBirth).toDate();
   }
 
-export const uploadProfileImage = (file, fileName) => 
-  async (dispatch, getState, {getFirebase, getFirestore}) => {
-      const firebase = getFirebase();
-      const firestore = getFirestore();
-      const user = firebase.auth().currentUser;
-      const path = `${user.uid}/user_images`;
-      const options = {
-          name: fileName
-      }
-
-      try {
-        // upload the file to firebase storage 
-        let uploadedFile = await firebase.uploadFile(path, file, null, options);
-        // get url of image 
-        let downloadURL = await uploadedFile.uploadTaskSnapshot.downloadURL; 
-        // get userdoc from firestore
-        let userDoc = await firestore.get(`users/${user.uid}`);
-        // check if user has photo, if not update profile with new image 
-        if(!userDoc.data().photoURL) {
-            //update firestore document 
-          await firebase.updateProfile({
-              photoURL: downloadURL
-          });
-            //update the profile inside of firebase authentication 
-          await user.updateProfile({
-              photoURL: downloadURL
-          });
-        };
-        // add the new phto as a new image in photo collection 
-        return await firestore.add({
-            collection: 'users', 
-            doc: user.uid,
-            subcollections: [{collection: 'photos'}],
-        }, {
-            name: fileName,
-            url: downloadURL
-        })
-      } catch (error) {
-          console.log(error)
-          throw new Error('Problem uploading photo'); 
-      }
+  try {
+    await firebase.updateProfile(updatedUser);
+    toastr.success("Success", "Profile updated!");
+  } catch (error) {
+    console.log(error);
   }
+};
 
-//No need for a reducer we will use firebase and its created  consts and reducers. 
+export const uploadProfileImage = (file, fileName) => async (
+  dispatch,
+  getState,
+  { getFirebase, getFirestore }
+) => {
+  const imageName = cuid();
+  const firebase = getFirebase();
+  const firestore = getFirestore();
+  const user = firebase.auth().currentUser;
+  const path = `${user.uid}/user_images`;
+  const options = {
+    name: imageName
+  };
+
+  try {
+    // upload the file to firebase storage
+    let uploadedFile = await firebase.uploadFile(path, file, null, options);
+    // get url of image
+    let downloadURL = await uploadedFile.uploadTaskSnapshot.downloadURL;
+    // get userdoc from firestore
+    let userDoc = await firestore.get(`users/${user.uid}`);
+    // check if user has photo, if not update profile with new image
+    if (!userDoc.data().photoURL) {
+      //update firestore document
+      await firebase.updateProfile({
+        photoURL: downloadURL
+      });
+      //update the profile inside of firebase authentication
+      await user.updateProfile({
+        photoURL: downloadURL
+      });
+    }
+    // add the new phto as a new image in photo collection
+    return await firestore.add(
+      {
+        collection: "users",
+        doc: user.uid,
+        subcollections: [{ collection: "photos" }]
+      },
+      {
+        name: imageName,
+        url: downloadURL
+      }
+    );
+  } catch (error) {
+    console.log(error);
+    throw new Error("Problem uploading photo");
+  }
+};
+
+//No need for a reducer we will use firebase and its created  consts and reducers.
