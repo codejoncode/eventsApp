e3d5e1423f95dd3fc3dcf45fffa75bf3353b70fc
codejoncode <jonathanjholloway@gmail.com>
2019-05-18 17:05:53 -0500

    added in lazy loader for the user detailed photos/ profile pics

--- client/src/Components/user/UserDetailed/UserDetailedPhotos.js
+++ client/src/Components/user/UserDetailed/UserDetailedPhotos.js
@@ -1,5 +1,7 @@
 import React from "react";
 import { Grid, Segment, Header, Image } from "semantic-ui-react";
+import LazyLoad from 'react-lazyload'
+import imagesObject from "../../../Images/imagesObject"
 const UserDetailedPhotos = ({ photos }) => {
   return (
     <Grid.Column width={12}>
@@ -8,7 +10,13 @@ const UserDetailedPhotos = ({ photos }) => {
 
         <Image.Group size="small">
           {photos &&
-            photos.map(photo => <Image key={photo.id} src={photo.url} />)}
+            photos.map(photo => 
+              // offset will wait until we can see 150 pixels to load it. if this property is on here offset = {-150}
+            <LazyLoad key={photo.id} height={150}  placeholder={<Image  src={imagesObject.user} />} >
+              <Image  src={photo.url} />
+
+            </LazyLoad>
+            )}
         </Image.Group>
       </Segment>
     </Grid.Column>
