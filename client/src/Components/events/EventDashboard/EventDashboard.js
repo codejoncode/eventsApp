d7d7f6b542446c5a13ed0222bbd524fb35c892ef
codejoncode <jonathanjholloway@gmail.com>
2019-05-19 16:49:51 -0500

    have paging and a new button if clicking more you can move forward. Currently the first two events appear clicking more allows for the next two events to appear.  this is capable because i comemnted out the where clause in eventActions to test out this functionality. useer expereience drastically needs improvement.

--- client/src/Components/events/EventDashboard/EventDashboard.js
+++ client/src/Components/events/EventDashboard/EventDashboard.js
@@ -1,7 +1,7 @@
 import React, { Component } from "react";
 import { connect } from "react-redux";
 import { firestoreConnect, isLoaded, isEmpty } from "react-redux-firebase";
-import { Grid } from "semantic-ui-react";
+import { Grid, Button } from "semantic-ui-react";
 import EventList from "../EventList/EventList.js";
 import { getEventsForDashboard } from "../EventList/eventActions";
 
@@ -19,9 +19,31 @@ const actions = {
 }; // passed done to connect to be aprt of props.
 
 class EventDashboard extends Component {
+  state ={
+    moreEvents: false 
+  }
+ async componentDidMount() {
+    let next = await this.props.getEventsForDashboard(); // contains query snapshot
+    console.log(next);
+    //currently have a limit of limit of 2
+    if( next && next.docs && next.docs.length > 1 ){
+      this.setState({
+        moreEvents: true
+      })
+    }
+  }
 
-  componentDidMount() {
-    this.props.getEventsForDashboard(); 
+  getNextEvents = async () => {
+    const {events} = this.props; 
+    let lastEvent = events && events[events.length - 1];
+    console.log(lastEvent);
+    let next = await this.props.getEventsForDashboard(lastEvent);
+    console.log(next); 
+    if( next && next.docs && next.docs.length <= 1 ){
+      this.setState({
+        moreEvents: false
+      })
+    }
   }
 
   handleDeleteEvent = eventId => () => {
@@ -35,6 +57,7 @@ class EventDashboard extends Component {
       <Grid>
         <Grid.Column width={10}>
           <EventList  events={events} />
+          <Button onClick={this.getNextEvents} disabled={!this.state.moreEvents} content = 'More' color='green' floated='right'/> 
         </Grid.Column>
         <Grid.Column width={6}>
           <EventActivity />
--- client/src/Components/events/EventList/eventActions.js
+++ client/src/Components/events/EventList/eventActions.js
@@ -10,9 +10,9 @@ import {
   asynActionError
 } from "../../async/asyncActions";
 import { fetchSampleData } from "../../../data/mockApi";
-import { createNewEvent } from '../../common/util/helpers';
-import moment from 'moment'; 
-import firebase from '../../../config/firebase';
+import { createNewEvent } from "../../common/util/helpers";
+import moment from "moment";
+import firebase from "../../../config/firebase";
 // export const fetchEvents = events => {
 //   return {
 //     type: FETCH_EVENTS,
@@ -21,21 +21,21 @@ import firebase from '../../../config/firebase';
 // };
 
 export const createEvent = event => {
-  return async (dispatch, getState, {getFirestore}) => {
-    const firestore = getFirestore(); 
-    const uid = await firestore.auth().currentUser.uid; 
+  return async (dispatch, getState, { getFirestore }) => {
+    const firestore = getFirestore();
+    const uid = await firestore.auth().currentUser.uid;
     const displayName = await getState().firebase.profile.displayName;
-    const user = {uid, displayName};
+    const user = { uid, displayName };
     const photoURL = getState().firebase.profile.photoURL;
     let newEvent = createNewEvent(user, photoURL, event);
     try {
       let createdEvent = await firestore.add(`events`, newEvent);
       await firestore.set(`event_attendee/${createdEvent.id}_${user.uid}`, {
-        eventID: createdEvent.id, 
+        eventID: createdEvent.id,
         userUid: user.uid,
         eventDate: event.date,
         host: true
-      })
+      });
       toastr.success("Success!", "Event has been created");
     } catch (error) {
       toastr.error("Oops", "Something went wrong");
@@ -44,14 +44,14 @@ export const createEvent = event => {
 };
 
 export const updateEvent = event => {
-  return async (dispatch, getState, {getFirestore} )=> {
-    const firestore = getFirestore(); 
-    if (event.date !== getState().firestore.ordered.events[0].date){
+  return async (dispatch, getState, { getFirestore }) => {
+    const firestore = getFirestore();
+    if (event.date !== getState().firestore.ordered.events[0].date) {
       //this checks prevents a reset to 1970
-      event.date = moment(event.date).toDate(); 
+      event.date = moment(event.date).toDate();
     }
     try {
-      await firestore.update(`events/${event.id}`, event); 
+      await firestore.update(`events/${event.id}`, event);
       toastr.success("Success!", "Event has been updated");
     } catch (error) {
       toastr.error("Oops", "Something went wrong");
@@ -59,41 +59,72 @@ export const updateEvent = event => {
   };
 };
 
-
-
-export const cancelToggle = (cancelled, eventId) => 
-  async (dispatch, getState, {getFirestore}) => {
-    const firestore = getFirestore();
-    const message = cancelled ? 'Are you sure you want to cancel the event?' : 'This will reactivate the event - are you sure'; 
-    try {
-      toastr.confirm(message, {
-        onOk: () => firestore.update(`events/${eventId}`, {
+export const cancelToggle = (cancelled, eventId) => async (
+  dispatch,
+  getState,
+  { getFirestore }
+) => {
+  const firestore = getFirestore();
+  const message = cancelled
+    ? "Are you sure you want to cancel the event?"
+    : "This will reactivate the event - are you sure";
+  try {
+    toastr.confirm(message, {
+      onOk: () =>
+        firestore.update(`events/${eventId}`, {
           cancelled
         })
-      })
-    } catch (error) {
-      console.log(error)
-    }
+    });
+  } catch (error) {
+    console.log(error);
   }
+};
 
-  export const getEventsForDashboard = () => 
-    async (dispatch, getState) => {
-      let today = new Date(Date.now());
-      const firestore = firebase.firestore();
-      const eventsQuery = firestore.collection('events').where('date','>=', today); 
-      console.log(eventsQuery)
-      try {
-        dispatch(asyncActionStart())
-        let querySnap = await eventsQuery.get()
-        let events = []; 
-        for (let i = 0; i< querySnap.docs.length; i++){
-          let evt = {...querySnap.docs[i].data(), id: querySnap.docs[i].id};
-          events.push(evt); 
-        }
-        dispatch({type: FETCH_EVENTS, payload: events})
-        dispatch(asyncActionFinish())
-      } catch (error) {
-        console.log(error)
-        dispatch(asynActionError())
-      }
+export const getEventsForDashboard = lastEvent => async (
+  dispatch,
+  getState
+) => {
+  let today = new Date(Date.now());
+  const firestore = firebase.firestore();
+  const eventsRef = firestore.collection("events");
+  try {
+    dispatch(asyncActionStart());
+    let startAfter =
+      lastEvent &&
+      (await firestore
+        .collection("events")
+        .doc(lastEvent.id)
+        .get());
+    let query;
+
+    lastEvent
+      ? (query = eventsRef
+          // .where("date", ">=", today)
+          .orderBy("date")
+          .startAfter(startAfter)
+          .limit(2))
+      : (query = eventsRef
+          // .where("date", ">=", today)
+          .orderBy("date")
+          .limit(2));
+
+    let querySnap = await query.get();
+
+    if (querySnap.docs.length === 0 ){
+      dispatch(asyncActionFinish())
+      return; 
     }
+
+    let events = [];
+    for (let i = 0; i < querySnap.docs.length; i++) {
+      let evt = { ...querySnap.docs[i].data(), id: querySnap.docs[i].id };
+      events.push(evt);
+    }
+    dispatch({ type: FETCH_EVENTS, payload: events });
+    dispatch(asyncActionFinish());
+    return querySnap; 
+  } catch (error) {
+    console.log(error);
+    dispatch(asynActionError());
+  }
+};
